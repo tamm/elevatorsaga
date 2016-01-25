b77c8cb4f95b59453eb9019baf98f5e7d70a3b93
tamm <tamm.sjodin@gmail.com>
2016-01-25 21:50:34 +0100

    Update elevatorsaga.js

--- elevatorsaga.js
+++ elevatorsaga.js
@@ -1,67 +1,46 @@
 {
     init: function(elevators, floors) {
-        var sendLeastBusyElevator = function(targetFloor){
-            var leastBusy = 0;
-            var leastBusyCount = null;
-
-            for (var i = 0; i < elevators.length; i++) {
-                var queue = elevbaseators[i].destinationQueue;
-                if ((queue.length < leastBusyCount || leastBusyCount === null) && elevators[i].loadFactor() < 0.4) {
-                    leastBusy = i;
-                    leastBusyCount = queue.length;
-                }
-            }
-            goToIfNotInQueue(elevators[leastBusy], targetFloor);
-        }
         var goToIfNotInQueue = function(elevator, targetFloor){
-            var queue = elevator.destinationQueue;
-            if (queue.indexOf(targetFloor) < 0) {
+            if (elevator.destinationQueue.indexOf(targetFloor) == -1) {
                 elevator.goToFloor(targetFloor);
-            } else {
-                return false; //already sending a lift
-            }
-        }
-        var goToMostImportantFloor = function (){
-            var mostImportantFloorCount = 0;
-            var pressedFloors = this.getPressedFloors();
-
-            if (pressedFloors.length > 0) {
-                var mostImportantFloor = pressedFloors[0];
-
-                for ( var floor in pressedFloors ) {
-                    if(floors[floor].buttonStates.up || floors[floor].buttonStates.down) {
-                        mostImportantFloor = floor;
-                    }
-                }
-            }
-
-            if(this.loadFactor() < 0.1) {
-                // Maybe use this elevator, since it's not full yet?
-            }
-            else 
-            {
-                goToIfNotInQueue(this, mostImportantFloor);
             }
         }
 
         for (var i = 0; i < elevators.length; i++) {
-            elevators[i].on("idle", goToMostImportantFloor);
+            elevators[i].on("idle", function() {
+                if (this.destinationQueue < 1) {
+                    this.goToFloor(0);
+                }
+            });
 
             elevators[i].on("floor_button_pressed", function(floorNum) {
-                this.floors[floorNum]++;
-                this.trigger("idle");
-            } );
+                goToIfNotInQueue(this, floorNum);
+            });
+
+            elevators[i].on("passing_floor", function(floorNum, direction) {
+                if (this.destinationQueue.indexOf(floorNum) > -1) {
+                    this.destinationQueue.splice(this.destinationQueue.indexOf(floorNum), 1);
+                    this.goToFloor(floorNum, true);
+                }
+            });
         }
+
         for (var i = 0; i < floors.length; i++) {
-            floors[i].on("up_button_pressed", function() {
-                sendLeastBusyElevator(this.level);
-            })
-            floors[i].on("down_button_pressed", function() {
-                sendLeastBusyElevator(this.level);
-            })
+            floors[i].on("up_button_pressed down_button_pressed", function() {
+                var leastBusy = 0;
+
+                for (var i = 0; i < elevators.length; i++) {
+                    if (elevators[i].destinationQueue.indexOf(this.level) > -1) {
+                        return;
+                    }
+                    if (elevators[i].destinationQueue.length < elevators[leastBusy].destinationQueue.length) {
+                        leastBusy = i;
+                    }
+                }
+
+                goToIfNotInQueue(elevators[leastBusy], this.level);
+            });
         }
     },
-    update: function(dt, elevators, floors) {
-        // We normally don't need to do anything here
-    }
+    update: function(dt, elevators, floors) { }
 }
